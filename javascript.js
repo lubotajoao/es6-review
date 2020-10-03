27d9c9dcb0bde26d96cb8dfb9f5e49a24b80bc2e
Lubota Ngola Joao <lubota4@gmail.com>
2020-10-03 04:49:08 -0300

    Filter Loop Excercise

--- javascript.js
+++ javascript.js
@@ -1,33 +1,34 @@
 /**
- * Exercise One
+ * Exercise Filter
  */
-
-// const paintings = [
-//   { name: "Mona Lisa", width: 200, height: 200 },
-//   { name: "The scream", width: 400, height: 600 },
-//   { name: "The last  supper", width: 600, height: 700 },
-// ];
-
-// const messages = paintings.map(function (painting) {
-//   return `The ${painting.name} is ${painting.width} X ${painting.height}`;
-// });
-
-// console.log(messages);
-
-/**
- * Exercise Tow
- */
-
-const cars = [
-  { name: "Ford", price: 200 },
-  { name: "Nissan", price: 500 },
-  { name: "Toyota", price: 600 },
+const channels = [
+  { name: "HBO", premium: true },
+  { name: "LIFE", premium: false },
+  { name: "Max", premium: true },
+  { name: "Cooking channel", premium: false },
+  { name: "WOBI", premium: false },
 ];
 
-//  Ford is 40000 rupies
+const user = {
+  name: "Lubota",
+  premium: true,
 
-const price = cars.map(function (car) {
-  return `${car.name} is ${car.price * 200} rupies`;
-});
+  premiumChannels: function () {
+    // GET THE PREMIUM CHANNELS IS "PREMIUM" IS true
+    const $this = this;
+    return channels.filter(function (channel) {
+      return channel.premium && $this.premium;
+    });
+  },
 
-console.log(price);
+  channels: function () {
+    // GET THE NON-PREMIUM CHANNELS
+    return channels.filter(function (channel) {
+      return channel.premium === false;
+    });
+  },
+};
+console.log(user.premiumChannels());
+// brings HBO and MAX
+console.log(user.channels());
+// brings LIFE, COOCKING CHANNEL AND WOBI
