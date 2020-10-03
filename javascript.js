f8034df370ec17a6040ae79fb753c4c72fae7b3d
Lubota Ngola Joao <lubota4@gmail.com>
2020-10-03 06:41:17 -0300

    Find Method

--- javascript.js
+++ javascript.js
@@ -1,34 +1,43 @@
 /**
- * Exercise Filter
+ * Find
  */
-const channels = [
-  { name: "HBO", premium: true },
-  { name: "LIFE", premium: false },
-  { name: "Max", premium: true },
-  { name: "Cooking channel", premium: false },
-  { name: "WOBI", premium: false },
+
+// Example: #01
+// const brands = [
+//   { name: "Apple", id: 1 },
+//   { name: "HP", id: 2 },
+//   { name: "Samsung", id: 3 },
+//   { name: "Samsung", id: 4 },
+// ];
+
+// const result = brands.find(function (brand) {
+//   return brand.name === "Samsung";
+// });
+
+// console.log(result);
+
+// Example: #02
+const cars = [
+  { brand: "Ford", price: 500, available: 2, type: "Sport Car" },
+  { brand: "Nissa", price: 200, available: 5, type: "Wagon" },
+  { brand: "Chevy", price: 300, available: 4, type: "Sport Car" },
+  { brand: "Porshe", price: 100, available: 1, type: "Urban" },
 ];
 
-const user = {
-  name: "Lubota",
-  premium: true,
+function getResoults() {
+  return cars.find(function (car) {
+    return car.price < price && car.available > 0 && car.type === type;
+  });
+}
 
-  premiumChannels: function () {
-    // GET THE PREMIUM CHANNELS IS "PREMIUM" IS true
-    const $this = this;
-    return channels.filter(function (channel) {
-      return channel.premium && $this.premium;
-    });
-  },
+document.querySelector(".search").addEventListener("click", function () {
+  let price = parseIn(document.querySelector("#price").value);
+  let type = document.querySelector("#type").value;
+  let results = getResoults(price, type);
 
-  channels: function () {
-    // GET THE NON-PREMIUM CHANNELS
-    return channels.filter(function (channel) {
-      return channel.premium === false;
-    });
-  },
-};
-console.log(user.premiumChannels());
-// brings HBO and MAX
-console.log(user.channels());
-// brings LIFE, COOCKING CHANNEL AND WOBI
+  if (results) {
+    console.log(`Found ${result.brand} fir $ ${results.price}`);
+  } else {
+    console.log(`Sorry, no car for you`);
+  }
+});
